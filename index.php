2a44f2ab62446608b3b6163078d3f2c27dfdbfb5
Bradley-D <Bradley-D@users.noreply.github.com>
2018-11-30 14:27:49 +1000

    [ ADDED ] some sidebar structure

--- index.php
+++ index.php
@@ -1,1 +1,135 @@
 <?php
+/**
+ * DGC Style Guide - This is where the magic gets pulled together.
+ *
+ * @since 1.0.0
+ */
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>DGC Style guide</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	</head>
+	<body>
+		<a id="skippy" class="sr-only sr-only-focusable" href="<!-- TODO -->">
+			<div class="container">
+				<span class="skiplink-text">Skip to main content</span>
+			</div>
+		</a>
+		<header class="navbar navbar-expand navbar-dark flex-column flex-md-row bd-navbar shadow-sm">
+			<div class="navbar-nav-scroll">
+				<h5 class="my-3 mr-md-auto text-dark font-weight-normal">Destination Gold Coast - Style Guide</h5>
+			</div>
+			<ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex">
+				<li class="">
+					<a id="small" data-size="420px" data-size-label="small" class="">
+						<span>Small</span></a>
+				</li>
+				<li class="">
+					<a id="medium" data-size="720px" data-size-label="medium" class="">
+						<span>Medium</span></a>
+				</li>
+				<li class="">
+					<a id="large" data-size="1280px" data-size-label="large" class="">
+						<span>Large</span></a>
+				</li>
+			</ul>
+		</header>
+		<div clas="container-fluid">
+			<div class="row">
+				<!-- SIDEBAR -->
+				<div class="col-md-2 d-none d-md-block bg-light sidebar">
+					<div class="sidebar-sticky">
+						<ul class="nav flex-column">
+							<li>DGC Brand Overview
+								<ul>
+									<li>Style Guide Introduction</li>
+									<li>DGC Tone of Voice</li>
+									<li>Brand Summary</li>
+									<li>DGC Logo & Devices
+										<ul>
+											<li>Usage</li>
+											<li>Colour</li>
+										</ul>
+									</li>
+									<li>DGC Typefaces
+										<ul>
+											<li>Primary</li>
+											<li>Secondary</li>
+											<li>Decorative</li>
+										</ul>
+									</li>
+									<li>DGC Photography</li>
+								</ul>
+							</li>
+							<li>DGC Print Branding
+								<ul>
+									<li>Introduction</li>
+									<li>Logo
+										<ul>
+											<li></li>
+											<li></li>
+											<li></li>
+										</ul>
+									</li>
+									<li>Colour Palette</li>
+									<li>Photography
+										<ul>
+											<li></li>
+											<li></li>
+											<li></li>
+										</ul>
+									</li>
+									<li>Typography
+										<ul>
+											<li>Headings</li>
+											<li>Subheadings</li>
+											<li>Body Copy</li>
+											<li>Bullet Points</li>
+											<li>Pull Out Quotes</li>
+											<li>Call to Actions</li>
+											<li>Special Characters</li>
+										</ul>
+									</li>
+									<li>Print Promotion
+										<ul>
+											<li>Advertising</li>
+											<li>Flyers</li>
+											<li>Brochures</li>
+											<li>Signage</li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+							<li>DGC Digital Branding / Website
+								<ul>
+									<li>Introduction</li>
+									<li>Foundation</li>
+									<li>Logo</li>
+									<li>Colour</li>
+									<li>Photography</li>
+									<li>Typography</li>
+									<li>Tables</li>
+									<li>Buttons</li>
+									<li>Links</li>
+									<li>Form Elements</li>
+									<li>Accessibility</li>
+								</ul>
+							</li>
+						<ul>
+					</div>
+				</div>
+				<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+				</main>
+			</div>
+		</div>
+	</body>
+</html>
