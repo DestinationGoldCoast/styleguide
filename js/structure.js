03552a8f6ba07fd016946816e356585283984459
Bradley-D <Bradley-D@users.noreply.github.com>
2019-03-13 13:59:39 +1000

    [ REMOVED ] js for device switch

--- js/structure.js
+++ js/structure.js
@@ -1,21 +1,5 @@
 jQuery.noConflict();
 (function($) {
-    $("li.device a").on("click", function() {
-        var id = $(this).attr('id');
-        switch (id) {
-            case 'small':
-                col = 'col-4 mr-auto ml-auto';
-                break;
-            case 'medium':
-                col = 'col-8 mr-auto ml-auto';
-                break;
-            default:
-                col = 'col-12';
-        }
-        $("#display-switch").removeClass().addClass(col);
-
-    });
-
     // Menu and sub menu
     $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
         var $subMenu = $(this).next('.dropdown-menu');
