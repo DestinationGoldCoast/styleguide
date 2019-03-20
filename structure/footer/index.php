a4cbdf6f42ab934bc8ec8a916db764de77ea5313
Bradley-D <Bradley-D@users.noreply.github.com>
2019-03-20 12:08:22 +1000

    [ UPDATE ] converted php to html with liquid include_relative

--- structure/footer/index.php
+++
@@ -1,4 +0,0 @@
-<div id="footer" class="footer">
-	<?php require_once 'partials/logo.html'; ?>
-	<?php require_once 'partials/url.html'; ?>
-</div>
--- structure/header/menu.php
+++
@@ -1,6 +0,0 @@
-<ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex">
-	<?php require_once 'structure/menu/brand.html'; ?>
-	<?php require_once 'structure/menu/print.html'; ?>
-	<?php require_once 'structure/menu/digital.html'; ?>
-</ul>
-
