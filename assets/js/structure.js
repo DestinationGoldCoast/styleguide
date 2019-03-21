82f0fe18d0ffac1f72ccf97ba74e94feefc83295
Bradley-D <Bradley-D@users.noreply.github.com>
2019-03-21 12:41:12 +1000

    [ ADDED ] images again

---
+++ assets/js/structure.js
@@ -0,0 +1,16 @@
+jQuery.noConflict();
+(function($) {
+    // Menu and sub menu
+    $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+        var $subMenu = $(this).next('.dropdown-menu');
+        $subMenu.toggleClass('show');
+
+        $(this).parents('.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+            $('.dropdown-submenu .show').removeClass('show');
+        });
+
+        return false;
+    });
+
+
+})(jQuery);
